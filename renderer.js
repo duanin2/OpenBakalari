814a3f04d62f2145ab21b6651bdf4116fffdeb5b
duanin2 <duanin2@gmail.com>
2023-10-27 17:29:16 +0200

    Start again

--- renderer.js
+++
@@ -1,5 +0,0 @@
-function login() {
-	let instance = document.getElementById("inst").value;
-	let user = document.getElementById("user").value;
-	let password = document.getElementById("pass").value;
-}
