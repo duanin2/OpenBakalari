6f9dae57af0363ea98b0e623012cd7cd3977b5b7
duanin2 <duanin2@gmail.com>
2023-10-28 16:58:03 +0200

    login

--- main.js
+++ main.js
@@ -1,40 +1,93 @@
-const {
-		QMainWindow,
-		QWidget,
-		QLabel,
-		QLineEdit,
-		QPushButton,
-		FlexLayout
-} = require("@nodegui/nodegui");
+const qt = require("@nodegui/nodegui");
+const https = require("https");
 
-const win = new QMainWindow();
+const qApp = qt.QApplication.instance();
+const win = new qt.QMainWindow();
 
-const centralWidget = new QWidget();
+const centralWidget = new qt.QWidget();
 centralWidget.setObjectName("root");
-const rootLayout = new FlexLayout();
+centralWidget.setInlineStyle("margin: auto; padding: 4px;");
+const rootLayout = new qt.FlexLayout();
 centralWidget.setLayout(rootLayout);
 
-const label = new QLabel();
-label.setObjectName("label");
+const label = new qt.QLabel();
+label.setInlineStyle("font-size: 16px;");
 label.setText("Login");
 
-const instanceField = new QLineEdit();
-instanceField.setObjectName("instance");
+const errorMessage = new qt.QLabel();
+errorMessage.setInlineStyle("color: red;");
+errorMessage.setDisabled(true);
+
+const instanceField = new qt.QLineEdit();
 instanceField.setPlaceholderText("Instance URL");
 
-const userField = new QLineEdit();
-userField.setObjectName("user");
+const userField = new qt.QLineEdit();
 userField.setPlaceholderText("Username");
 
-const passField = new QLineEdit();
-passField.setObjectName("pass");
+const passField = new qt.QLineEdit();
 passField.setPlaceholderText("Password");
 
-const loginButton = new QPushButton();
-loginButton.setObjectName("login");
+const loginButton = new qt.QPushButton();
 loginButton.setText("Login");
+loginButton.addEventListener('clicked', () => {
+		const instance = instanceField.text();
+		const username = userField.text();
+		const password = passField.text();
+
+		if (!instance || !username || !password) {
+				errorMessage.setText("A required field is missing.");
+				errorMessage.setEnabled(true);
+				return;
+		}
+
+		errorMessage.setText("");
+		errorMessage.setDisabled(true);
+
+		let accessToken;
+		let refreshToken;
+
+		const loginReq = https.request({
+				hostname: instance,
+				path: "/api/login",
+				method: 'POST',
+				headers: {
+						'Content-Type': "application/x-www-form-urlencoded"
+				}
+		}, (res) => {
+				let json = "";
+				
+				res.on('data', (chunk) => {
+						json += chunk;
+				});
+
+				res.on('end', () => {
+						json = JSON.parse(json);
+
+						if (res.statusCode == 400) {
+								errorMessage.setText(json.error_description);
+								errorMessage.setEnabled(true);
+								return;
+						}
+						
+						accessToken = json.access_token;
+						refreshToken = json.refresh_token;
+						
+						qApp.quit();
+				});
+		});
+
+		loginReq.on('error', (err) => {
+				errorMessage.setText(err);
+				errorMessage.setEnabled(true);
+				return;
+		});
+
+		loginReq.write(`client_id=ANDR&grant_type=password&username=${username}&password=${password}`);
+		loginReq.end();
+});
 
 rootLayout.addWidget(label);
+rootLayout.addWidget(errorMessage);
 rootLayout.addWidget(instanceField);
 rootLayout.addWidget(userField);
 rootLayout.addWidget(passField);
