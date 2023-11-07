bd1c62fb08af63f303132bfef0bc735bc6fca8a0
duanin2 <duanin2@gmail.com>
2023-11-07 22:33:55 +0100

    Development Environment

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A Nix-flake-based OpenBakaláři development environment and packaging.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        android_sdk.accept_license = true;
+        allowUnfree = true;
+      };
+    };
+  in {
+    devShells = {
+      default = let
+        jdk = pkgs.openjdk;
+        qt = pkgs.qt6.full;
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          includeEmulator = false;
+          platformVersions = [ "26" "27" "28" "29" "30" "31" "32" "33" "34" ];
+          includeSources = false;
+          includeSystemImages = false;
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          includeNDK = true;
+        };
+      in pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          qt
+          jdk
+          androidComposition.androidsdk
+          
+          qtcreator
+        ];
+
+        JAVA_HOME = "${jdk}/lib/openjdk";
+
+        ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+        ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+      };
+    };
+  });
+}
